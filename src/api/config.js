dfc70081686e217cbbdbdaadc04c893e7d5f59a9
nicky <690984649@qq.com>
2018-03-15 18:46:34 +0800

    begin

---
+++ src/api/config.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: Nicky-Liu 
+ * @Date: 2018-03-15 17:31:42 
+ * @Last Modified by: Nicky-Liu
+ * @Last Modified time: 2018-03-15 17:57:45
+ */
+
+export const commonParams = {
+  g_tk: 5381,
+  inCharset: "utf-8",
+  outCharset: "utf-8",
+  notice: 0
+}
+
+export const options = {
+  param: 'jsonpCallback'
+}
+
+export const ERR_OK = 0
